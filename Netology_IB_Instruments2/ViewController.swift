bbb9bea84ec0efc0b4db770dce2ec928e00b9303
JuliaKornisina <julia20m@mail.ru>
2022-02-17 00:44:50 +0300

    ios-homeworks

---
+++ Netology_IB_Instruments2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments2
+//
+//  Created by Юлия Корнишина on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
